ca72601e87d5d2411d867f36c5e01ecc5ec7a5f4
Ray Xiang <rayray.xiang@gmail.com>
2025-08-14 19:57:47 -0700

    Initialized spring boot app

---
+++ backend/src/main/java/com/rayxiang/backupapp/BackupappApplication.java
@@ -0,0 +1,13 @@
+package com.rayxiang.backupapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackupappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rayxiang/backupapp/BackupappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rayxiang.backupapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackupappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
